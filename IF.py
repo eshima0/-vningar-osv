09581800352826d205e59fd671314e9af943883e
eshima0 <linus.sveed@elev.ga.ntig.se>
2021-10-04 13:20:32 +0200

    🎉 övning 10/4/2021

---
+++ IF.py
@@ -0,0 +1,12 @@
+# real asf fråga
+print("is this real or?\nyeah? yeah. this real asf. yeah this real. yeah\nyeah ")
+
+svar = ""
+
+while svar != "nah" and svar != "naw" and svar != "no" and svar != "aw hell naw":
+
+    svar = input("real?\n ")
+    if svar == "yeah" and "yea" and "balle" and "ya" and "yah" and "yea cunt we're gonna make it":
+        print("thats real asf")
+        
+       
